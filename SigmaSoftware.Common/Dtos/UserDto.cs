e9a41e32816c7bdce07d2c915a1a0c6fe0d2270e
asadbek20005-rgb <154343160+asadbek20005-rgb@users.noreply.github.com>
2025-04-16 12:11:41 +0500

    Done

---
+++ SigmaSoftware.Common/Dtos/UserDto.cs
@@ -0,0 +1,21 @@
+﻿namespace SigmaSoftware.Common.Dtos;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+
+    public string? PhoneNumber { get; set; } = string.Empty;
+
+    public string? CallTime { get; set; } = string.Empty;
+
+    public string? LinkedInUrl { get; set; } = string.Empty;
+
+    public string? GitHubUrl { get; set; } = string.Empty;
+}
---
+++ SigmaSoftware.Common/Models/UpdateUserModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SigmaSoftware.Common.Models;
+
+public class UpdateUserModel
+{
+   public string? FirstName { get; set; } = string.Empty;
+
+
+    public string? LastName { get; set; } = string.Empty;
+
+    public string? Email { get; set; } = string.Empty;
+
+
+    public string? PhoneNumber { get; set; } = string.Empty;
+
+    public string? CallTime { get; set; } = string.Empty;
+
+    public string? LinkedInUrl { get; set; } = string.Empty;
+
+    public string? GitHubUrl { get; set; } = string.Empty;
+    public string Comments { get; set; } = string.Empty;
+
+}
