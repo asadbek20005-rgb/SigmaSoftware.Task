b339092c2b23c18e2a8cac18651c2d3fb4f05525
asadbek20005-rgb <154343160+asadbek20005-rgb@users.noreply.github.com>
2025-04-16 12:01:55 +0500

    Creating Repostory

---
+++ SigmaSoftware.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SigmaSoftware.Data.Entites;
+
+namespace SigmaSoftware.Data.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; } 
+}
