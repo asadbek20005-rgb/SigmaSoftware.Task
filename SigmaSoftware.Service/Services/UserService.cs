99d5c94a6943ae096780848c4a38695c7acbe217
asadbek20005-rgb <154343160+asadbek20005-rgb@users.noreply.github.com>
2025-04-16 13:16:13 +0500

    Done

---
+++ SigmaSoftware.Service/Services/UserService.cs
@@ -0,0 +1,63 @@
+﻿using Mapster;
+using Microsoft.Extensions.Caching.Memory;
+using SigmaSoftware.Common.Dtos;
+using SigmaSoftware.Common.Models;
+using SigmaSoftware.Data.Entites;
+using SigmaSoftware.Data.RepositoryContracts;
+using SigmaSoftware.Service.ServiceContracts;
+using StatusGeneric;
+
+namespace SigmaSoftware.Service.Services;
+
+public class UserService(IBaseRepository<User> userRepository, IMemoryCache memoryCache) : StatusGenericHandler, IUserService
+{
+    private readonly IMemoryCache _memoryCache = memoryCache;
+    private const string CacheKey = "user";
+    private readonly IBaseRepository<User> _userRepository = userRepository;
+    public async Task AddUser(AddUserModel model)
+    {
+        bool areThereUsers = _memoryCache.TryGetValue(CacheKey, out List<User>? users);
+        if (!areThereUsers)
+        {
+            users = _userRepository.GetAll().ToList();
+            _memoryCache.Set(CacheKey, users);
+        }
+        if (users is null || !users.Any())
+        {
+            AddError("No users found in the cache.");
+            return;
+        }
+
+        var user = users.Where(u => u.Email == model.Email)
+            .FirstOrDefault();
+
+        if (user is not null)
+        {
+            var updatedUser = model.Adapt<UpdateUserModel>();
+            await UpdateUser(user.Id, updatedUser);
+        }
+
+        var newUser = model.Adapt<User>();
+        await _userRepository.Add(newUser);
+        await _userRepository.SaveChanges();
+    }
+
+    public List<UserDto> GetAllUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDto?> GetUserById(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateUser(Guid userId, UpdateUserModel model)
+    {
+
+        throw new NotImplementedException();
+
+    }
+
+
+}
